11ebda9c21fc09cb41515088126f027dbf61eb56
Prabhu Jay <prabhupj@gmail.com>
2022-03-13 12:10:38 -0400

    Initial Commit

---
+++ science/3d-solar-system/script.js
@@ -0,0 +1,65 @@
+/*
+MIT License
+
+Copyright (c) 2022 Prabaharan Jayabalan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+$(window).load(function(){
+
+  var body = $("body"),
+      universe = $("#universe"),
+      solarsys = $("#solar-system");
+
+  var init = function() {
+    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+      $(this).removeClass('hide-UI').addClass("set-speed");
+      $(this).dequeue();
+    });
+  };
+
+  var setView = function(view) { universe.removeClass().addClass(view); };
+
+  $("#toggle-data").click(function(e) {
+    body.toggleClass("data-open data-close");
+    e.preventDefault();
+  });
+
+  $("#toggle-controls").click(function(e) {
+    body.toggleClass("controls-open controls-close");
+    e.preventDefault();
+  });
+
+  $("#data a").click(function(e) {
+    var ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+  $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
+  $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
+  $(".set-size").click(function() { setView("scale-s set-size"); });
+  $(".set-distance").click(function() { setView("scale-d set-distance"); });
+
+  init();
+
+});
